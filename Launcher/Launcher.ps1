80eadcb33182f01eb5b24b1e011b7844ee740c7b
DrewTheGiraffe <87342933+DrewTheGiraffe@users.noreply.github.com>
2021-07-29 10:32:41 -0600

    Update Launcher.ps1
    Added Dynamic URLs that should update as new commits are made by contributors. 
    Thanks to https://gitcdn.link/
    -Drew 07/29/2021

--- Launcher/Launcher.ps1
+++ Launcher/Launcher.ps1
@@ -272,12 +272,12 @@ If ($Setupbackend -eq $true){
             Paste the permalink for any file in the URL box at that website.
             Just make sure you use production url option only and it will work fine in powershell
         #>
-        $AppScriptURL = "https://raw.githubusercontent.com/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/ApplicationsGUI.ps1"
-        $HostnameSURL = "https://raw.githubusercontent.com/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/HostnameGUI.ps1"
+        $AppScriptURL = "https://gitcdn.link/repo/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/ApplicationsGUI.ps1onsGUI.ps1"
+        $HostnameSURL = "https://gitcdn.link/repo/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/HostnameGUI.ps1"
         $ADUSERPS1URL = ""
-        $GeneralTechU = "https://raw.githubusercontent.com/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/GeneralTechGUI.ps1"
-        $ZipScriptURL = "https://raw.githubusercontent.com/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/ZipExtractorGUI.ps1"
-        $AllInOneURL  = "https://raw.githubusercontent.com/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/Allinone.ps1"
+        $GeneralTechU = "https://gitcdn.link/repo/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/GeneralTechGUI.ps1"
+        $ZipScriptURL = "https://gitcdn.link/repo/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/ZipExtractorGUI.ps1"
+        $AllInOneURL  = "https://gitcdn.link/repo/DrewTheGiraffe/Powershell-Launcher-GUI/main/Launcher/Dependencies/Allinone.ps1"
         
 
 
